84c71a861e30f429060443bedd5b7d58808aefa4
Mattia <mattia.belletti@gmail.com>
2017-05-10 18:49:32 +0200

    Added handler test.

---
+++ todoapi/handler.spec.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const handler = require('./handler');
+
+describe('handler', () => {
+    describe('#getTodos', () => {
+        it('Should return a response with an empty JSON list.', (done) => {
+            handler.getTodos('event', {}, (err, response) => {
+                try {
+                    assert.equal(err, null);
+                    assert.equal(response.statusCode, 200);
+                    assert.deepEqual(JSON.parse(response.body), []);
+                    done();
+                } catch(e) {
+                    done(e);
+                }
+            });
+        });
+    });
+});
