7aea889cf4f54561c958180f6d1a44dfa7d1094f
Mattia <mattia.belletti@gmail.com>
2017-05-11 18:33:01 +0200

    todos and integration tests.

---
+++ tests/integration-todos-dynamo.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const assert = require('assert');
+const moment = require('moment');
+const aws = require('aws-sdk');
+const todos = require('../todoapi/todos');
+aws.config.update({ region: 'eu-central-1' });
+
+const dynamodb = new aws.DynamoDB();
+
+describe('todos', () => {
+    const emptyDb = () => new Promise((resolve, reject) => dynamodb.scan({
+        TableName: "TodosTest",
+        ExpressionAttributeNames: {
+            "#UN": "username",
+            "#TS": "timestamp"
+        },
+        ProjectionExpression: "#UN, #TS"
+    }, (err, result) => err ? reject(err) : result.Items.length == 0 ? resolve() : dynamodb.batchWriteItem({
+        RequestItems: {
+            "TodosTest": result.Items.map(entry => ({
+                DeleteRequest: {
+                    Key: {
+                        username: entry.username,
+                        timestamp: entry.timestamp
+                    }
+                }
+            }))
+        }
+    }, (err, result) => err ? reject(err) : resolve()))
+    );
+
+    const delayPromise = delay => new Promise((resolve) => setTimeout(resolve, delay));
+
+    const waitRepeat = (promiseFunction, delay = 250, numRepeats = 8) => {
+        return promiseFunction()
+            .catch(e => numRepeats <= 0 ?
+                Promise.reject(e) :
+                delayPromise(delay).then(() => waitRepeat(promiseFunction, delay, numRepeats - 1)));
+    }
+
+    it('gets no todo if the DB is empty', () => {
+        // empty the DB
+        return emptyDb().then(() => todos.getTodos("RedGlow").then(result => {
+            assert.equal(result.length, 0);
+        }));
+    });
+
+    it('gets one todo after it is added', () => {
+        return emptyDb()
+            .then(() => todos.addTodo("RedGlow", "Title", "Content"))
+            .then(() => waitRepeat(() => todos.getTodos("RedGlow").then(result => {
+                assert.equal(result.length, 1);
+                var entry = result[0];
+                assert.equal(entry.username, "RedGlow");
+                assert.equal(entry.title, "Title");
+                assert.equal(entry.content, "Content");
+                assert.equal(entry.timestamp < moment.utc().valueOf(), true, `not true that ${entry.timestamp} < ${moment.utc().valueOf()}`);
+            })));
+    });
+
+    it('gets no todos after one is added and consequently removed', () => {
+        return emptyDb()
+            .then(() => todos.addTodo("RedGlow", "Title", "Content"))
+            .then(() => waitRepeat(() => todos.getTodos("RedGlow").then(result => {
+                assert.equal(result.length, 1);
+                return todos.removeTodo("RedGlow", result[0].timestamp).then(() =>
+                    waitRepeat(() => todos.getTodos("RedGlow").then(result => {
+                        assert.equal(result.length, 0);
+                    }))
+                );
+            })))
+    });
+});
--- todoapi/todos.js
+++ todoapi/todos.js
@@ -1,16 +1,11 @@
 'use strict';
 
-var aws = require('aws-sdk');
+const aws = require('aws-sdk');
+const moment = require('moment');
 
-aws.config.update({region: 'eu-central-1'});
+aws.config.update({ region: 'eu-central-1' });
 
 const dynamodb = new aws.DynamoDB();
-
-/*
- * Schema:
- * username [partition key]: {username}
- * timestamp [sort key]: {timestamp}
- */
 
 /**
  * Get the list of todos for given username.
@@ -21,15 +16,59 @@ const dynamodb = new aws.DynamoDB();
 module.exports.getTodos = (username) => new Promise((resolve, reject) => dynamodb.query({
     TableName: "TodosTest",
     KeyConditionExpression: "username = :un",
-    ProjectionExpression: "username",
+    ProjectionExpression: "username,title,content,#TS",
     ExpressionAttributeValues: {
         ":un": {
             S: username
         }
+    },
+    ExpressionAttributeNames: {
+        "#TS": "timestamp"
     }
-}, (err, data) => err ? reject(err) : resolve(data.Items)
+}, (err, data) => err ? reject(err) : resolve(data.Items.map(item => ({
+    username: item.username.S,
+    title: item.title.S,
+    content: item.content.S,
+    timestamp: parseInt(item.timestamp.N)
+})))
 ));
 
-module.exports.addTodo = (userId, todo) => Promise.resolve();
+/**
+ * Add a new TODO.
+ * 
+ * @param   {string} username
+ * @param   {string} title
+ * @param   {string} content
+ * @return  {Promise}
+ */
+module.exports.addTodo = (username, title, content) => new Promise((resolve, reject) => {
+    dynamodb.putItem({
+        TableName: "TodosTest",
+        Item: {
+            username: { S: username },
+            timestamp: { N: moment.utc().valueOf().toString() },
+            title: { S: title },
+            content: { S: content }
+        }
+    }, (err, data) => err ? reject(err) : resolve());
+});
 
-module.exports.removeTodo = (userId, todoId) => Promise.resolve();
+/**
+ * Remove a TODO.
+ * 
+ * @param   {string} username
+ * @param   {moment} timestamp
+ */
+module.exports.removeTodo = (username, timestamp) => new Promise((resolve, reject) => {
+    dynamodb.deleteItem({
+        Key: {
+            "username": {
+                S: username
+            },
+            "timestamp": {
+                N: timestamp.toString()
+            }
+        },
+        TableName: "TodosTest"
+    }, (err, data) => err ? reject(err) : resolve());
+});
