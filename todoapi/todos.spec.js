2bf08600e398bdfa97284980227444964f037f7d
Mattia <mattia.belletti@gmail.com>
2017-05-11 18:40:05 +0200

    Removing objects tested through integration.

--- todoapi/todos.spec.js
+++ todoapi/todos.spec.js
@@ -1,10 +1,1 @@
-const assert = require('assert');
-const todos = require('./todos');
-
-describe('todos', () => {
-    describe('#getTodos', () => {
-        it('should return an empty list at startup.',
-            () => todos.getTodos().then(result => assert.deepStrictEqual(result, []))
-        );
-    });
-});
+// fully tested through integration tests
